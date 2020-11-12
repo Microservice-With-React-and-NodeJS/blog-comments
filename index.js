15006235c4f82c2551421e42e4e5e217a4ce3d72
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-13 10:52:55 +1100

    Impliment event bus, emit event and received event

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 //create app
 const app = express();
@@ -16,7 +17,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
@@ -24,7 +25,24 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  //emit event whenever something interesting happening
+  //axios
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  });
+
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received comment event:", req.body.type);
+  res.send({});
 });
 
 //listen to app
