749ae14111cf12fad22f4975cd7ba13c84397ac1
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-13 06:18:23 +1100

    nothing

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const commentsByPostId = {};
 
 //create routes
 app.get("/posts/:id/comments", (req, res) => {
-  res.send(commentsByPostId[req.params.id]);
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post("/posts/:id/comments", (req, res) => {
